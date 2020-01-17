b98acda908025c599e8e687ccdca5b1469e0b9f9
Chase Grainger <chase.grainger2134@gmail.com>
2020-01-17 14:19:12 -0500

    removed comments

--- src/index.js
+++ src/index.js
@@ -9,7 +9,3 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
-
-// matthew
-
-// chase's comment
