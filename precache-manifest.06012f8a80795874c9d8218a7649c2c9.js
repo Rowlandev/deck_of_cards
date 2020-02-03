db79b6e9817257437ba84584fa85a6cf7474f314
Chase Grainger <chasegrainger@Chases-MBP.iwunet.indwes.edu>
2020-02-03 13:55:22 -0500

    Updates

---
+++ precache-manifest.06012f8a80795874c9d8218a7649c2c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "925326d479d407f9b7069c3baa6acaff",
+    "url": "/Rowlandev/deck_of_cards/index.html"
+  },
+  {
+    "revision": "0e9acf31ebdf1e8eb564",
+    "url": "/Rowlandev/deck_of_cards/static/js/2.1d404eea.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Rowlandev/deck_of_cards/static/js/2.1d404eea.chunk.js.LICENSE"
+  },
+  {
+    "revision": "00a9cd6ea3449f948f2e",
+    "url": "/Rowlandev/deck_of_cards/static/js/main.807f6ae1.chunk.js"
+  },
+  {
+    "revision": "477395fb8695eca5b718",
+    "url": "/Rowlandev/deck_of_cards/static/js/runtime-main.efd11cbc.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Rowlandev/deck_of_cards/precache-manifest.06012f8a80795874c9d8218a7649c2c9.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Rowlandev/deck_of_cards/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
