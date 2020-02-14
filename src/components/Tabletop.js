a76aa5307181e057750294bbee6a6a82e48e551d
Chase Grainger <chse.grainger2134@gmail.com>
2020-02-14 14:13:51 -0500

    removed merge comments

--- src/components/Tabletop.js
+++ src/components/Tabletop.js
@@ -5,11 +5,7 @@ class Tabletop extends React.Component {
   render() {
     return (
       <div>
-<<<<<<< HEAD
         <PlayingCard suite="diamond" value="6" color="red"/>
-=======
-        <PlayingCard suite="heart" value="A" color="red"/>
->>>>>>> 1c96edba53ceaa05bd04bc2cc0d1fb8c7b7615c7
       </div>
     );
   }
