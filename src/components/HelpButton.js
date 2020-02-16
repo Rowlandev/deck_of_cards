11d3b4e98e3b4e1d9491dd0d1ae91a0785222c0c
Chase Grainger <chse.grainger2134@gmail.com>
2020-02-15 20:41:08 -0500

    added help message

--- src/components/HelpButton.js
+++ src/components/HelpButton.js
@@ -3,10 +3,33 @@ import HelpIcon from "../images/icons/passive-help.png";
 import "../css/HelpButton.css";
 
 class HelpButton extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      showing: this.props.showing
+    };
+  }
+
+  showHelp = () => {
+    this.setState({
+      showing: !this.state.showing
+    });
+    console.log(this.state.showing);
+  }
+
   render() {
     return (
       <div className="help-icon">
-        <img src={HelpIcon} alt="help icon" id="help-icon" onmouseover="this.src='passive-help.png'" onmouseout="this.src='active.png'"/>
+        <img src={HelpIcon} alt="help icon" id="help-icon" onClick={this.showHelp}/>
+
+        {this.state.showing &&
+          <div id="help-message">
+            Welcome to our deck of cards simulation. It is still being progressed,
+            so come back soon to see more functionality!
+          </div>
+        }
+
       </div>
     );
   }
--- src/components/Tabletop.js
+++ src/components/Tabletop.js
@@ -3,10 +3,18 @@ import PlayingCard from "./PlayingCard.js";
 import HelpButton from "./HelpButton.js";
 
 class Tabletop extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      showingHelpMessage: false
+    };
+  }
+
   render() {
     return (
       <div>
-        <HelpButton/>
+        <HelpButton showing={this.state.showingHelpMessage}/>
         <PlayingCard suite="heart" value="5" color="red"/>
       </div>
     );
