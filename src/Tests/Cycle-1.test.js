2faa2acf40ac9a80285103af61fa36c03cd16d71
Chase Grainger <chse.grainger2134@gmail.com>
2020-02-22 17:22:37 -0500

    added more test support

---
+++ src/Tests/Cycle-1.test.js
@@ -0,0 +1,37 @@
+// SYSTEM IMPORTS
+import React from "react";
+import ReactDOM from "react-dom";
+
+// TESTING TOOL IMPORTS
+import Enzyme, { shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import '@testing-library/jest-dom/extend-expect';
+import { render } from "@testing-library/react";
+
+// COMPONENTS TO BE TESTED
+import App from "../App.js";
+import HelpButton from "../components/HelpButton.js";
+import PlayingCard from "../components/PlayingCard.js";
+import Tabletop from "../components/Tabletop.js";
+
+/* CONFIGURE ENZYME */
+Enzyme.configure({ adapter: new Adapter() });
+
+/* SET OF TESTS FOR FIRST DEVELOPMENT CYCLE */
+describe('CYCLE 1 TESTS', () => {
+
+  var helpButtonWrapper = shallow(<HelpButton showing={false}/>); // get App component
+  var playingCardWrapper = shallow(<PlayingCard suite="heart" value="5" color="red"/>); // get SidePanel component
+  var tabletopWrapper = shallow(<Tabletop/>);
+
+  var helpButtonInstacne = helpButtonWrapper.instance();
+  var playingCardInstance = playingCardWrapper.instance();
+  var tabletopInstance = tabletopWrapper.instance();
+
+  /* TEST #1 - COMPONENTS RENDER CORRECTLY */
+  it('COMPONENTS RENDER CORRECTLY', () => {
+    const { asFragment } = render(<App/>); // GENERATES SNAPSHOT OF TABLETOP
+    expect(asFragment()).toMatchSnapshot(); // CHECKS IF SNAPSHOT MATCHES RECENTLY RENDERED COMPONENT
+  })
+
+});
