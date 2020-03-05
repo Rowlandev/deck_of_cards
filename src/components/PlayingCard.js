cdc4c26b665dde572797be640ee6b2ce2397d3c5
Matthew Heck <matthewck@Matthews-MacBook-Air.local>
2020-03-05 14:35:19 -0500

    added sandbox mode, entire deck, and cleaned up code

--- src/components/PlayingCard.js
+++ src/components/PlayingCard.js
@@ -1,195 +1,193 @@
 import React from "react";
 import "../css/PlayingCard.css";
 
-// import red corner suite icons
-import RedClubCorner from "../images/red-corners/club.png";
+// import red corner suit icons
 import RedDiamondCorner from "../images/red-corners/diamond.png";
 import RedHeartCorner from "../images/red-corners/heart.png";
-import RedSpadeCorner from "../images/red-corners/spade.png";
 
-// import black corner suite icons
+// import black corner suit icons
 import BlackClubCorner from "../images/black-corners/club.png";
-import BlackDiamondCorner from "../images/black-corners/diamond.png";
-import BlackHeartCorner from "../images/black-corners/heart.png";
 import BlackSpadeCorner from "../images/black-corners/spade.png";
 
 // import red large icons
-import RedClubLarge from "../images/red-large/club.png";
 import RedDiamondLarge from "../images/red-large/diamond.png";
 import RedHeartLarge from "../images/red-large/heart.png";
-import RedSpadeLarge from "../images/red-large/spade.png";
 
 // import black large icons
 import BlackClubLarge from "../images/black-large/club.png";
-import BlackDiamondLarge from "../images/black-large/diamond.png";
-import BlackHeartLarge from "../images/black-large/heart.png";
 import BlackSpadeLarge from "../images/black-large/spade.png";
 
 // import GuyFiere from "../images/backs/guy-fieri.jpg"
 import Prof from "../images/backs/prof.jpg"
 
-class PlayingCard extends React.Component {
+class PlayingCard extends React.Component{
 
   constructor(props) {
     super(props);
     this.state = {
-      suite: this.props.suite,
+      suit: this.props.suit,
       value: this.props.value,
-      color: this.props.color,
-      side: "back"
+      top: 50,
+      left: 50,
+      mouseDownX:0,
+      mouseDownY:0,
+      side: "back",
+      clicked: false
     };
   }
 
-  toggleCardSide = () => {
+  cardClicked = (e) => {
+    this.mouseDownX = e.clientX;
+    this.mouseDownY = e.clientY;
+    this.setState({
+      clicked: true
+    })
+  }
 
-    var newSide;
+  cardReleased = (e) => {
+    var newSide = this.state.side;  //in case it is not changed
+    var newTop = this.state.top;
+    var newLeft = this.state.left;
+    var x = e.clientX;
+    var y = e.clientY;
 
-    if (this.state.side === "back") {
-      newSide = "front";
-    } else {
-      newSide = "back";
+
+    if (this.state.clicked === true){
+      if (x === this.mouseDownX && y === this.mouseDownY) {
+        if (this.state.side === "back") {
+          newSide = "front";
+        } else {
+          newSide = "back";
+        }
+      } else {
+        newTop = this.state.top + (y - this.mouseDownY);
+        newLeft = this.state.left + (x - this.mouseDownX);
+      }
     }
 
     this.setState({
-      suite: this.props.suite,
+      suit: this.props.suit,
       value: this.props.value,
-      color: this.props.color,
-      side: newSide
+      top: newTop,
+      left: newLeft,
+      side: newSide,
+      clicked: false
     });
   }
 
   render() {
 
     // define variables for card values
-    var topLeftSuite;
+    var topLeftSuit;
     var topLeftValue;
-    var bottomRightSuite;
+    var bottomRightSuit;
     var bottomRightValue;
-    var largeSuite;
+    var largeSuit;
+    var offset = {top: this.state.top + "px",
+                  left: this.state.left + "px"
+                }
 
-    // assign card red suite
-    if (this.state.color === "red") {
+    // assign card red suit
 
-      if (this.state.suite === "heart") {
-        topLeftSuite = <img src={RedHeartCorner} alt="suite of hearts" id="top-left-suite"/>;
-        bottomRightSuite = <img src={RedHeartCorner} alt="suite of hearts" id="bottom-right-suite"/>;
-        largeSuite = <img src={RedHeartLarge} alt="large heart" id="large-suite"/>
-      } else if (this.state.suite === "spade") {
-          topLeftSuite = <img src={RedSpadeCorner} alt="suite of spades" id="top-left-suite"/>;
-          bottomRightSuite = <img src={RedSpadeCorner} alt="suite of spades" id="bottom-right-suite"/>;
-          largeSuite = <img src={RedSpadeLarge} alt="large heart" id="large-suite"/>
-      } else if (this.state.suite === "diamond") {
-        topLeftSuite = <img src={RedDiamondCorner} alt="suite of diamonds" id="top-left-suite"/>;
-        bottomRightSuite = <img src={RedDiamondCorner} alt="suite of diamonds" id="bottom-right-suite"/>;
-        largeSuite = <img src={RedDiamondLarge} alt="large heart" id="large-suite"/>
-      } else if (this.state.suite === "club") {
-        topLeftSuite = <img src={RedClubCorner} alt="suite of clubs" id="top-left-suite"/>;
-        bottomRightSuite = <img src={RedClubCorner} alt="suite of clubs" id="bottom-right-suite"/>;
-        largeSuite = <img src={RedClubLarge} alt="large heart" id="large-suite"/>
-      }
-
-    // assign card black suite
-    } else {
-
-      if (this.state.suite === "heart") {
-        topLeftSuite = <img src={BlackHeartCorner} alt="suite of hearts" id="top-left-suite"/>;
-        bottomRightSuite = <img src={BlackHeartCorner} alt="suite of hearts" id="bottom-right-suite"/>;
-        largeSuite = <img src={BlackHeartLarge} alt="large heart" id="large-suite"/>
-      } else if (this.state.suite === "spade") {
-          topLeftSuite = <img src={BlackSpadeCorner} alt="suite of spades" id="top-left-suite"/>;
-          bottomRightSuite = <img src={BlackSpadeCorner} alt="suite of spades" id="bottom-right-suite"/>;
-          largeSuite = <img src={BlackSpadeLarge} alt="large heart" id="large-suite"/>
-      } else if (this.state.suite === "diamond") {
-        topLeftSuite = <img src={BlackDiamondCorner} alt="suite of diamonds" id="top-left-suite"/>;
-        bottomRightSuite = <img src={BlackDiamondCorner} alt="suite of diamonds" id="bottom-right-suite"/>;
-        largeSuite = <img src={BlackDiamondLarge} alt="large heart" id="large-suite"/>
-      } else if (this.state.suite === "club") {
-        topLeftSuite = <img src={BlackClubCorner} alt="suite of clubs" id="top-left-suite"/>;
-        bottomRightSuite = <img src={BlackClubCorner} alt="suite of clubs" id="bottom-right-suite"/>;
-        largeSuite = <img src={BlackClubLarge} alt="large heart" id="large-suite"/>
-      }
-
+    if (this.state.suit === "heart") {
+      topLeftSuit = <img src={RedHeartCorner} alt="suit of hearts" id="top-left-suit"/>;
+      bottomRightSuit = <img src={RedHeartCorner} alt="suit of hearts" id="bottom-right-suit"/>;
+      largeSuit = <img src={RedHeartLarge} alt="large heart" id="large-suit"/>
+    } else if (this.state.suit === "diamond") {
+      topLeftSuit = <img src={RedDiamondCorner} alt="suit of diamonds" id="top-left-suit"/>;
+      bottomRightSuit = <img src={RedDiamondCorner} alt="suit of diamonds" id="bottom-right-suit"/>;
+      largeSuit = <img src={RedDiamondLarge} alt="large heart" id="large-suit"/>
+    } else if (this.state.suit === "spade") {
+        topLeftSuit = <img src={BlackSpadeCorner} alt="suit of spades" id="top-left-suit"/>;
+        bottomRightSuit = <img src={BlackSpadeCorner} alt="suit of spades" id="bottom-right-suit"/>;
+        largeSuit = <img src={BlackSpadeLarge} alt="large heart" id="large-suit"/>
+    } else if (this.state.suit === "club") {
+      topLeftSuit = <img src={BlackClubCorner} alt="suit of clubs" id="top-left-suit"/>;
+      bottomRightSuit = <img src={BlackClubCorner} alt="suit of clubs" id="bottom-right-suit"/>;
+      largeSuit = <img src={BlackClubLarge} alt="large heart" id="large-suit"/>
     }
 
     // assign card value based on props
     switch (this.state.value) {
       case "A":
-        topLeftValue = <p className={this.state.color} id="top-left-value">A</p>;
-        bottomRightValue = <p className={this.state.color} id="bottom-right-value">A</p>;
+        topLeftValue = <p className={this.state.suit} id="top-left-value">A</p>;
+        bottomRightValue = <p className={this.state.suit} id="bottom-right-value">A</p>;
         break;
       case "2":
-        topLeftValue = <p className={this.state.color} id="top-left-value">2</p>;
-        bottomRightValue = <p className={this.state.color} id="bottom-right-value">2</p>;
+        topLeftValue = <p className={this.state.suit} id="top-left-value">2</p>;
+        bottomRightValue = <p className={this.state.suit} id="bottom-right-value">2</p>;
         break;
       case "3":
-        topLeftValue = <p className={this.state.color} id="top-left-value">3</p>;
-        bottomRightValue = <p className={this.state.color} id="bottom-right-value">3</p>;
+        topLeftValue = <p className={this.state.suit} id="top-left-value">3</p>;
+        bottomRightValue = <p className={this.state.suit} id="bottom-right-value">3</p>;
         break;
       case "4":
-        topLeftValue = <p className={this.state.color} id="top-left-value">4</p>;
-        bottomRightValue = <p className={this.state.color} id="bottom-right-value">4</p>;
+        topLeftValue = <p className={this.state.suit} id="top-left-value">4</p>;
+        bottomRightValue = <p className={this.state.suit} id="bottom-right-value">4</p>;
         break;
       case "5":
-        topLeftValue = <p className={this.state.color} id="top-left-value">5</p>;
-        bottomRightValue = <p className={this.state.color} id="bottom-right-value">5</p>;
+        topLeftValue = <p className={this.state.suit} id="top-left-value">5</p>;
+        bottomRightValue = <p className={this.state.suit} id="bottom-right-value">5</p>;
         break;
       case "6":
-        topLeftValue = <p className={this.state.color} id="top-left-value">6</p>;
-        bottomRightValue = <p className={this.state.color} id="bottom-right-value">6</p>;
+        topLeftValue = <p className={this.state.suit} id="top-left-value">6</p>;
+        bottomRightValue = <p className={this.state.suit} id="bottom-right-value">6</p>;
         break;
       case "7":
-        topLeftValue = <p className={this.state.color} id="top-left-value">7</p>;
-        bottomRightValue = <p className={this.state.color} id="bottom-right-value">7</p>;
+        topLeftValue = <p className={this.state.suit} id="top-left-value">7</p>;
+        bottomRightValue = <p className={this.state.suit} id="bottom-right-value">7</p>;
         break;
       case "8":
-        topLeftValue = <p className={this.state.color} id="top-left-value">8</p>;
-        bottomRightValue = <p className={this.state.color} id="bottom-right-value">8</p>;
+        topLeftValue = <p className={this.state.suit} id="top-left-value">8</p>;
+        bottomRightValue = <p className={this.state.suit} id="bottom-right-value">8</p>;
         break;
       case "9":
-        topLeftValue = <p className={this.state.color} id="top-left-value">9</p>;
-        bottomRightValue = <p className={this.state.color} id="bottom-right-value">9</p>;
+        topLeftValue = <p className={this.state.suit} id="top-left-value">9</p>;
+        bottomRightValue = <p className={this.state.suit} id="bottom-right-value">9</p>;
         break;
       case "10":
-        topLeftValue = <p className={this.state.color} id="top-left-value">10</p>;
-        bottomRightValue = <p className={this.state.color} id="bottom-right-value">10</p>;
+        topLeftValue = <p className={this.state.suit} id="top-left-value">10</p>;
+        bottomRightValue = <p className={this.state.suit} id="bottom-right-value">10</p>;
         break;
       case "Jack":
-        topLeftValue = <p className={this.state.color} id="top-left-value">J</p>;
-        bottomRightValue = <p className={this.state.color} id="bottom-right-value">J</p>;
+        topLeftValue = <p className={this.state.suit} id="top-left-value">J</p>;
+        bottomRightValue = <p className={this.state.suit} id="bottom-right-value">J</p>;
         break;
       case "Queen":
-        topLeftValue = <p className={this.state.color} id="top-left-value">Q</p>;
-        bottomRightValue = <p className={this.state.color} id="bottom-right-value">Q</p>;
+        topLeftValue = <p className={this.state.suit} id="top-left-value">Q</p>;
+        bottomRightValue = <p className={this.state.suit} id="bottom-right-value">Q</p>;
         break;
       case "King":
-        topLeftValue = <p className={this.state.color} id="top-left-value">K</p>;
-        bottomRightValue = <p className={this.state.color} id="bottom-right-value">K</p>;
+        topLeftValue = <p className={this.state.suit} id="top-left-value">K</p>;
+        bottomRightValue = <p className={this.state.suit} id="bottom-right-value">K</p>;
         break;
       case "Joker":
-        topLeftValue = <p className={this.state.color} id="top-left-value">JOKER</p>;
-        bottomRightValue = <p className={this.state.color} id="bottom-right-value">JOKER</p>;
+        topLeftValue = <p className={this.state.suit} id="top-left-value">JOKER</p>;
+        bottomRightValue = <p className={this.state.suit} id="bottom-right-value">JOKER</p>;
         break;
       default:
         break;
     }
 
-    return (
 
+
+
+
+    return (
       <div>
-      { this.state.side === "back" &&
-        <div id="back-of-card" data-testid="card" onClick={this.toggleCardSide}>
+      {this.state.side === "back" &&
+        <div id="back-of-card" style={offset} data-testid="card" onMouseDown={(e) => this.cardClicked(e)} onMouseUp={(e) => this.cardReleased(e)}>
           <img src={Prof} alt="guy fieri" id="back-image"/>
         </div>
       }
 
-      {
-        this.state.side === "front" &&
-        <div id="playing-card" data-testid="card" onClick={this.toggleCardSide}>
-          {topLeftSuite}
-          {bottomRightSuite}
+      {this.state.side === "front" &&
+        <div id="playing-card" style={offset} data-testid="card" onMouseDown={(e) => this.cardClicked(e)} onMouseUp={(e) => this.cardReleased(e)}>
+          {topLeftSuit}
+          {bottomRightSuit}
           {topLeftValue}
           {bottomRightValue}
-          {largeSuite}
+          {largeSuit}
         </div>
       }
       </div>
--- src/components/Tabletop.js
+++ src/components/Tabletop.js
@@ -2,6 +2,113 @@ import React from "react";
 import PlayingCard from "./PlayingCard.js";
 import HelpButton from "./HelpButton.js";
 
+var deck = {
+  card1Suit: "spade",
+  card1Val: "A",
+  card2Suit: "club",
+  card2Val: "2",
+  card3Suit: "club",
+  card3Val: "3",
+  card4Suit: "club",
+  card4Val: "4",
+  card5Suit: "club",
+  card5Val: "5",
+  card6Suit: "club",
+  card6Val: "6",
+  card7Suit: "club",
+  card7Val: "7",
+  card8Suit: "club",
+  card8Val: "8",
+  card9Suit: "club",
+  card9Val: "9",
+  card10Suit: "club",
+  card10Val: "10",
+  card11Suit: "club",
+  card11Val: "Jack",
+  card12Suit: "club",
+  card12Val: "Queen",
+  card13Suit: "club",
+  card13Val: "King",
+  card14Suit: "spade",
+  card14Val: "A",
+  card15Suit: "spade",
+  card15Val: "2",
+  card16Suit: "spade",
+  card16Val: "3",
+  card17Suit: "spade",
+  card17Val: "4",
+  card18Suit: "spade",
+  card18Val: "5",
+  card19Suit: "spade",
+  card19Val: "6",
+  card20Suit: "spade",
+  card20Val: "7",
+  card21Suit: "spade",
+  card21Val: "8",
+  card22Suit: "spade",
+  card22Val: "9",
+  card23Suit: "spade",
+  card23Val: "10",
+  card24Suit: "spade",
+  card24Val: "Jack",
+  card25Suit: "spade",
+  card25Val: "Queen",
+  card26Suit: "spade",
+  card26Val: "King",
+  card27Suit: "heart",
+  card27Val: "A",
+  card28Suit: "heart",
+  card28Val: "2",
+  card29Suit: "heart",
+  card29Val: "3",
+  card30Suit: "heart",
+  card30Val: "4",
+  card31Suit: "heart",
+  card31Val: "5",
+  card32Suit: "heart",
+  card32Val: "6",
+  card33Suit: "heart",
+  card33Val: "7",
+  card34Suit: "heart",
+  card34Val: "8",
+  card35Suit: "heart",
+  card35Val: "9",
+  card36Suit: "heart",
+  card36Val: "10",
+  card37Suit: "heart",
+  card37Val: "Jack",
+  card38Suit: "heart",
+  card38Val: "Queen",
+  card39Suit: "heart",
+  card39Val: "King",
+  card40Suit: "diamond",
+  card40Val: "A",
+  card41Suit: "diamond",
+  card41Val: "2",
+  card42Suit: "diamond",
+  card42Val: "3",
+  card43Suit: "diamond",
+  card43Val: "4",
+  card44Suit: "diamond",
+  card44Val: "5",
+  card45Suit: "diamond",
+  card45Val: "6",
+  card46Suit: "diamond",
+  card46Val: "7",
+  card47Suit: "diamond",
+  card47Val: "8",
+  card48Suit: "diamond",
+  card48Val: "9",
+  card49Suit: "diamond",
+  card49Val: "10",
+  card50Suit: "diamond",
+  card50Val: "Jack",
+  card51Suit: "diamond",
+  card51Val: "Queen",
+  card52Suit: "diamond",
+  card52Val: "King"
+}
+
 class Tabletop extends React.Component {
 
   constructor(props) {
@@ -11,11 +118,63 @@ class Tabletop extends React.Component {
     };
   }
 
+
   render() {
     return (
       <div>
         <HelpButton showing={this.state.showingHelpMessage}/>
-        <PlayingCard suite="heart" value="5" color="red"/>
+        <PlayingCard suit={deck.card1Suit} value={deck.card1Val}/>
+        <PlayingCard suit={deck.card2Suit} value={deck.card2Val}/>
+        <PlayingCard suit={deck.card3Suit} value={deck.card3Val}/>
+        <PlayingCard suit={deck.card4Suit} value={deck.card4Val}/>
+        <PlayingCard suit={deck.card5Suit} value={deck.card5Val}/>
+        <PlayingCard suit={deck.card6Suit} value={deck.card6Val}/>
+        <PlayingCard suit={deck.card7Suit} value={deck.card7Val}/>
+        <PlayingCard suit={deck.card8Suit} value={deck.card8Val}/>
+        <PlayingCard suit={deck.card9Suit} value={deck.card9Val}/>
+        <PlayingCard suit={deck.card10Suit} value={deck.card10Val}/>
+        <PlayingCard suit={deck.card11Suit} value={deck.card11Val}/>
+        <PlayingCard suit={deck.card12Suit} value={deck.card12Val}/>
+        <PlayingCard suit={deck.card13Suit} value={deck.card13Val}/>
+        <PlayingCard suit={deck.card14Suit} value={deck.card14Val}/>
+        <PlayingCard suit={deck.card15Suit} value={deck.card15Val}/>
+        <PlayingCard suit={deck.card16Suit} value={deck.card16Val}/>
+        <PlayingCard suit={deck.card17Suit} value={deck.card17Val}/>
+        <PlayingCard suit={deck.card18Suit} value={deck.card18Val}/>
+        <PlayingCard suit={deck.card19Suit} value={deck.card19Val}/>
+        <PlayingCard suit={deck.card20Suit} value={deck.card20Val}/>
+        <PlayingCard suit={deck.card21Suit} value={deck.card21Val}/>
+        <PlayingCard suit={deck.card22Suit} value={deck.card22Val}/>
+        <PlayingCard suit={deck.card23Suit} value={deck.card23Val}/>
+        <PlayingCard suit={deck.card24Suit} value={deck.card24Val}/>
+        <PlayingCard suit={deck.card25Suit} value={deck.card25Val}/>
+        <PlayingCard suit={deck.card26Suit} value={deck.card26Val}/>
+        <PlayingCard suit={deck.card27Suit} value={deck.card27Val}/>
+        <PlayingCard suit={deck.card28Suit} value={deck.card28Val}/>
+        <PlayingCard suit={deck.card29Suit} value={deck.card29Val}/>
+        <PlayingCard suit={deck.card30Suit} value={deck.card30Val}/>
+        <PlayingCard suit={deck.card31Suit} value={deck.card31Val}/>
+        <PlayingCard suit={deck.card32Suit} value={deck.card32Val}/>
+        <PlayingCard suit={deck.card33Suit} value={deck.card33Val}/>
+        <PlayingCard suit={deck.card34Suit} value={deck.card34Val}/>
+        <PlayingCard suit={deck.card35Suit} value={deck.card35Val}/>
+        <PlayingCard suit={deck.card36Suit} value={deck.card36Val}/>
+        <PlayingCard suit={deck.card37Suit} value={deck.card37Val}/>
+        <PlayingCard suit={deck.card38Suit} value={deck.card38Val}/>
+        <PlayingCard suit={deck.card39Suit} value={deck.card39Val}/>
+        <PlayingCard suit={deck.card40Suit} value={deck.card40Val}/>
+        <PlayingCard suit={deck.card41Suit} value={deck.card41Val}/>
+        <PlayingCard suit={deck.card42Suit} value={deck.card42Val}/>
+        <PlayingCard suit={deck.card43Suit} value={deck.card43Val}/>
+        <PlayingCard suit={deck.card44Suit} value={deck.card44Val}/>
+        <PlayingCard suit={deck.card45Suit} value={deck.card45Val}/>
+        <PlayingCard suit={deck.card46Suit} value={deck.card46Val}/>
+        <PlayingCard suit={deck.card47Suit} value={deck.card47Val}/>
+        <PlayingCard suit={deck.card48Suit} value={deck.card48Val}/>
+        <PlayingCard suit={deck.card49Suit} value={deck.card49Val}/>
+        <PlayingCard suit={deck.card50Suit} value={deck.card50Val}/>
+        <PlayingCard suit={deck.card51Suit} value={deck.card51Val}/>
+        <PlayingCard suit={deck.card52Suit} value={deck.card52Val}/>
       </div>
     );
   }
